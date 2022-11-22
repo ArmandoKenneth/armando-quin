86b9ffef2159f140aaa99b13974778a563f20f65
Armando <armando.kenneth@junglescout.com>
2022-11-22 17:43:10 -0300

    Adding Antd in case there are complex components needed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,20 @@
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes, FC } from 'react';
 import styled from 'styled-components'
 
-interface IButton extends ButtonHTMLAttributes<HTMLElement> {
-  color: string
+import { ButtonProps } from 'antd/lib/button/button';
+
+import { Button } from 'antd';
+
+interface IButton extends ButtonProps {
 }
 
-export const SampleButton = styled.button<IButton>`
-  background-color: ${props => props.color};
-  border-color: ${props => props.color};
-  border-radius: 3px;
-  padding: 10px;
+// export const SampleButton = styled.button<IButton>`
+//   background-color: ${props => props.color};
+//   border-color: ${props => props.color};
+//   border-radius: 3px;
+//   padding: 10px;
+// `
+
+export const SampleButton: React.FunctionComponent<IButton> = styled(Button)`
+  background-color: red;
 `
